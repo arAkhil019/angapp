d2e2032a94c71dc5a29fbe473942f3fc1acc7eb5
arAkhil019 <114822421+arAkhil019@users.noreply.github.com>
2024-06-28 13:11:50 +0530

    added home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angapp';
+  heading = 'Welcome to Angular App';
+  // constructor() {
+  //   setTimeout(() => {
+  //     this.heading = 'Welcome to New Changed Angular App';
+  //   }, 2000);
+  // }
 }
